ace03fc3bd138174800e4ee6a8acd13806f406f8
idwer@localhost <idwer@localhost>
2021-12-03 02:27:56 +0100

    remove whitespace in solution (part 1) for puzzle 02

--- puzzle_02/solution_02-part-01.py
+++ puzzle_02/solution_02-part-01.py
@@ -4,10 +4,10 @@ import sys
 def do(file):
     pos_vertical = 0
     pos_horizontal = 0
-    
+
     with open(file) as infile:
         rows = [n for n in infile.read().splitlines()]
-        
+
         for _, e in enumerate(rows):
             line = e.split(' ')
             if 'forward' in line[0]:
@@ -17,7 +17,7 @@ def do(file):
             if 'up' in line[0]:
                 pos_vertical -= int(line[1])
 
-    print(f"What do you get if you multiply your final horizontal position by your final depth? {pos_horizontal * pos_vertical}")             
+    print(f"What do you get if you multiply your final horizontal position by your final depth? {pos_horizontal * pos_vertical}")
 
 
 if __name__ == '__main__':
