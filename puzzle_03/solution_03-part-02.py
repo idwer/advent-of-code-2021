551e3bcf5e35ba727dd075a9ce9f263401a99908
idwer@localhost <idwer@localhost>
2021-12-05 03:05:35 +0100

    solution for puzzle 03, part 02

---
+++ puzzle_03/solution_03-part-02.py
@@ -0,0 +1,54 @@
+def get_co2_scrubber_rating(r: list, line_width: int, column: int) -> list:
+    list_with_ones_common = [elem for elem in r if elem[column] == '1']
+    list_with_zeros_common = [elem for elem in r if elem[column] == '0']
+
+    ret = []
+
+    if len(list_with_zeros_common) > len(list_with_ones_common):
+        ret = list_with_ones_common
+    else:
+        ret = list_with_zeros_common
+
+    if len(ret) == 2:
+        if ret[0] < ret[1]:
+            ret = [ret[1]]
+        else:
+            ret = [ret[1]]
+        return ret
+
+    return get_co2_scrubber_rating(ret, line_width, column + 1)
+
+
+def get_oxygen_generator_rating(r: list, line_width: int, column: int) -> list:
+    list_with_ones_common = [elem for elem in r if elem[column] == '1']
+    list_with_zeros_common = [elem for elem in r if elem[column] == '0']
+
+    ret = []
+
+    if len(list_with_ones_common) > len(list_with_zeros_common):
+        ret = list_with_ones_common
+    else:
+        ret = list_with_zeros_common
+
+    if len(ret) == 2:
+        if ret[0] < ret[1]:
+            ret = [ret[1]]
+        else:
+            ret = [ret[1]]
+        return ret
+
+    return get_oxygen_generator_rating(ret, line_width, column + 1)
+
+
+def solution() -> int:
+    with open('input', 'r') as infile:
+        rows = [n for n in infile.read().splitlines()]
+        infile.close()
+
+        line_width = len(rows[0])
+
+    return int(get_co2_scrubber_rating(rows, line_width, 0)[0], 2) * int(get_oxygen_generator_rating(rows, line_width, 0)[0], 2)
+
+
+if __name__ == '__main__':
+    print(f"What is the life support rating of the submarine? {solution()}")
