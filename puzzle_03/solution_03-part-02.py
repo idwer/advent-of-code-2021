d6586cca4f5cd0c40087a5f5431b59facb319011
idwer@localhost <idwer@localhost>
2021-12-08 22:58:04 +0100

    simplify logic around recursion for puzzle 03, part 02, unfinished puzzle

--- puzzle_03/solution_03-part-02.py
+++ puzzle_03/solution_03-part-02.py
@@ -5,31 +5,24 @@ def get_life_support_generator_rating(get_rating_for_co2: bool, r: list, line_wi
     ret = []
 
     if get_rating_for_co2:
-        if len(list_with_ones_common) < len(list_with_zeros_common):
-            ret = list_with_ones_common
-        elif len(list_with_ones_common) == len(list_with_zeros_common):
+        if len(list_with_zeros_common) <= len(list_with_ones_common):
             ret = list_with_zeros_common
         else:
-            ret = list_with_zeros_common
-    else:
-        if len(list_with_ones_common) > len(list_with_zeros_common):
             ret = list_with_ones_common
-        elif len(list_with_ones_common) == len(list_with_zeros_common):
+    else:
+        if len(list_with_ones_common) >= len(list_with_zeros_common):
             ret = list_with_ones_common
         else:
             ret = list_with_zeros_common
 
     if len(ret) > 1:
-        if get_rating_for_co2:
-            return get_life_support_generator_rating(True, ret, line_width, column + 1)
-        else:
-            return get_life_support_generator_rating(False, ret, line_width, column + 1)
+        return get_life_support_generator_rating(get_rating_for_co2, ret, line_width, column + 1)
 
     return ret
 
 
-def solution() -> int:
-    with open('input', 'r') as infile:
+def solution(filename: str) -> int:
+    with open(filename, 'r') as infile:
         rows = [n for n in infile.read().splitlines()]
         infile.close()
 
@@ -39,4 +32,5 @@ def solution() -> int:
 
 
 if __name__ == '__main__':
-    print(f"What is the life support rating of the submarine? {solution()}")
+    # print(f"What is the life support rating of the submarine? {solution('test_input')}")
+    print(f"What is the life support rating of the submarine? {solution('input')}")
