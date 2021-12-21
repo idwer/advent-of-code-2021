26ee301468524fe98271c0f393b7af878b11ebe6
idwer@localhost <idwer@localhost>
2021-12-21 13:25:17 +0100

    store puzzle_01/input puzzle_02/input puzzle_03/input puzzle_01/test_input puzzle_02/test_input puzzle_03/test_input

--- puzzle_04/Board.py
+++ puzzle_04/Board.py
@@ -1,6 +1,63 @@
 class Board:
     tuples = []
     score = 0
+    winning_number_trigger = 0
+    input_board_height = 0
+
+    def get_sum_of_unmarked_numbers(self) -> int:
+        ret = 0
+
+        for t in self.tuples:
+            if not t[1]:
+                ret += t[0]
+
+        return ret
+
+    def mark_number(self, number: int) -> None:
+    # def mark_number(self, number: int, input_board_height: int) -> None:
+    #     self.input_board_height = input_board_height
+    # def mark_number(self, number: int) -> None:
+        for e in self.tuples:
+            if e[0] is number:
+                e[1] = True
+
+                # if self.has_winning_column(input_board_height) or self.has_winning_row(input_board_height):
+                #     return
+
+    def get_marked_numbers(self) -> list:
+        return [t for t in self.tuples if t[1]]
+
+    def has_winning_column(self, input_board_height: int) -> bool:
+        score = 0
+
+        for i in range(0, input_board_height):
+            # print(f"column: {i + 1}")
+            for n in range(0, input_board_height ** 2, input_board_height):
+                # print(self.tuples[i + n])
+                if self.tuples[i + n][1]:
+                    score += 1
+            if score is input_board_height:
+                return True
+
+        return False
+
+    def has_winning_row(self, input_board_height: int) -> bool:
+        score = 0
+
+        for n in range(0, input_board_height ** 2, input_board_height):
+            # for i in range(0, len(self.tuples[0])):
+            for i in range(0, input_board_height):
+                # print(self.tuples[i + n])
+                if self.tuples[n + i][1]:
+                    score += 1
+            if score is input_board_height:
+                return True
+            # print(self.tuples[n + i])
+
+        return False
+
+    def get_score(self) -> int:
+        return self.get_sum_of_unmarked_numbers() * self.winning_number_trigger
 
     def parse_list(self, rows: list) -> list:
         t = []
@@ -11,5 +68,9 @@ class Board:
                 t.append([elem, False])
         return t
 
-    def __init__(self, numbers: list):
+    def __init__(self, numbers: list) -> None:
+    # def __init__(self, numbers: list, input_board_height: int) -> None:
+    #     self.input_board_height = input_board_height
+        print(f"constructor in class Board, numbers = {numbers}")
+        # print(numbers[0].split())
         self.tuples = self.parse_list(numbers)
--- puzzle_04/solution_04.py
+++ puzzle_04/solution_04.py
@@ -1,6 +1,9 @@
 from Board import Board
 
 
+input_board_height = 0
+
+
 def get_number_of_boards(rows: list, block_size: int) -> int:
     return int(len(rows[1:]) / block_size)
 
@@ -48,28 +51,67 @@ def solution(filename: str) -> int:
 
     # take whitespace into account
     boards = generate_list_of_boards(number_of_boards, board_data, input_board_height + 1)
+    # print(boards[0].tuples[0])
+    # print(boards[0].tuples[5])
+    # print(boards[0].tuples[10])
+    # print(boards[0].tuples[15])
+    # print(boards[0].tuples[20])
 
     drawn_numbers = get_drawn_number(rows)
+
+    print(f"number of boards: {number_of_boards}")
+    print(f"drawn numbers: {drawn_numbers}")
 
     round = 0
     index = 0
     cap = input_board_height
     done = False
 
-    while round <= number_of_boards:
-        print(draw_numbers_per_round(drawn_numbers, index, cap, done))
+    # while round <= number_of_boards:
+    while round < number_of_boards:
+        numbers_per_round = draw_numbers_per_round(drawn_numbers, index, cap, done)
+
+        print(f"Round {round + 1}: {numbers_per_round}")
+        [boards[round].mark_number(number) for number in numbers_per_round]
+
+        # for board in boards:
+        #     # [board.mark_number(number) for number in numbers_per_round]
+        #     # [board.mark_number(number, input_board_height) for number in numbers_per_round]
+        #     for number in numbers_per_round:
+        #         board.mark_number(number)
+        #         # board.mark_number(number, input_board_height)
+        #         if board.has_winning_column(input_board_height):
+        #             # or board.has_winning_row(input_board_height):
+        #             print("yay")
+        #             # return
+        #         if board.has_winning_row(input_board_height):
+        #             print("omg")
+        #             # return
+
+        # print(boards[round].has_winning_column(input_board_height))
 
         round += 1
         index = cap
         cap += input_board_height + round
 
-        if round is number_of_boards:
-            done = True
-            print(draw_numbers_per_round(drawn_numbers, index, cap, done))
-            break
+        # if round is number_of_boards:
+        #     done = True
+        #     return
+
+    # for board in boards:
+    #     for x in range(0, len(board.tuples), 5):
+    #         print(board.get_winning_row(x, x + 5))
+
+    # print(boards[0].has_winning_column(input_board_height))
+    # print(boards[1].has_winning_column(input_board_height))
+    # print(boards[2].has_winning_column(input_board_height))
+
+    for board in boards:
+        print(board.get_marked_numbers())
 
     return score
 
 
 if __name__ == '__main__':
-    print(f"What will your final score be if you choose that board? {solution('input')}")
+    print(f"What will your final score be if you choose that board? {solution('test_input')}")
+    # print(f"What will your final score be if you choose that board? {solution('input')}")
