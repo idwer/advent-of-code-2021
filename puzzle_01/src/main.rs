92570a2fa93ac6eb5f3084cff4959c7642b950c4
idwer@localhost <idwer@localhost>
2022-07-01 19:25:01 +0200

    rust approach: fix console output

--- puzzle_01/src/main.rs
+++ puzzle_01/src/main.rs
@@ -29,16 +29,20 @@ fn main() {
     let mut result = solve_puzzle_01_input();
 
     assert_eq!(result, 1752);
+    println!("Solution for puzzle 01: {}", result);
 
     result = solve_puzzle_01_test_input();
 
     assert_eq!(result, 7);
+    println!("Solution for puzzle 01, test input: {}", result);
 
     result = solve_puzzle_02_input();
 
     assert_eq!(result, 1781);
+    println!("Solution for puzzle 02: {}", result);
 
     result = solve_puzzle_02_test_input();
 
     assert_eq!(result, 5);
+    println!("Solution for puzzle 02, test input: {}", result);
 }
