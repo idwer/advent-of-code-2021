b521c86f8da42f64d85c20e59e3716a240410b88
idwer@localhost <idwer@localhost>
2022-07-01 21:46:13 +0200

    rust, puzzle 01: reuse sample set (reuse filename)

--- puzzle_01/src/main.rs
+++ puzzle_01/src/main.rs
@@ -5,11 +5,10 @@ mod part_02;
 
 fn main() {
     let args:Vec<String> = env::args().collect();
-    let filename_part_1 = &args[1];
-    let filename_part_2 = &args[2];
+    let filename = &args[1];
 
-    let result_part_1 = part_01::solve_puzzle_01(filename_part_1.to_string());
-    let result_part_2 = part_02::solve_puzzle_02(filename_part_2.to_string());
+    let result_part_1 = part_01::solve_puzzle_01(filename.to_string());
+    let result_part_2 = part_02::solve_puzzle_02(filename.to_string());
 
     println!("Solution for part 1: {}", result_part_1);
     println!("Solution for part 2: {}", result_part_2);
