da74c75e96f5809a6fb97b433fb677d58b17f368
idwer@localhost <idwer@localhost>
2022-07-01 21:19:05 +0200

    rust: move code for puzzle 01 to separate file

--- puzzle_01/src/main.rs
+++ puzzle_01/src/main.rs
@@ -1,31 +1,12 @@
 use std::env;
-use std::fs;
 
-fn solve_puzzle_01(filename: String) -> i64 {
-    let mut increased = 0;
-
-    let input = fs::read_to_string(filename).expect("error opening file");
-
-    let mut numbers: Vec<i32> = Vec::new();
-
-    for line in input.lines() {
-        numbers.push(line.parse::<i32>().unwrap());
-    }
-
-    for (pos, _) in numbers.iter().enumerate() {
-        if pos > 0 && numbers[pos - 1] < numbers[pos] {
-                increased += 1;
-        }
-    }
-
-    increased
-}
+mod puzzle_01;
 
 fn main() {
     let args:Vec<String> = env::args().collect();
     let filename = &args[1];
 
-    let result = solve_puzzle_01(filename.to_string());
+    let result = puzzle_01::solve_puzzle_01(filename.to_string());
 
     println!("Solution for puzzle 01: {}", result);
 }
