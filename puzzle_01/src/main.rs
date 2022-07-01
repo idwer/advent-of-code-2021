b1623d56e0534719ef6d693c5e7ed775d6acbd22
idwer@localhost <idwer@localhost>
2022-07-01 21:40:51 +0200

    rust: prepare part 2 of puzzle 01

--- puzzle_01/src/main.rs
+++ puzzle_01/src/main.rs
@@ -1,12 +1,16 @@
 use std::env;
 
 mod puzzle_01;
+mod puzzle_02;
 
 fn main() {
     let args:Vec<String> = env::args().collect();
-    let filename = &args[1];
+    let filename_part_1 = &args[1];
+    let filename_part_2 = &args[2];
 
-    let result = puzzle_01::solve_puzzle_01(filename.to_string());
+    let result_part_1 = puzzle_01::solve_puzzle_01(filename_part_1.to_string());
+    let result_part_2 = puzzle_02::solve_puzzle_02(filename_part_2.to_string());
 
-    println!("Solution for puzzle 01: {}", result);
+    println!("Solution for part 1: {}", result_part_1);
+    println!("Solution for part 2: {}", result_part_2);
 }
---
+++ puzzle_01/src/puzzle_02.rs
@@ -0,0 +1,21 @@
+use std::fs;
+
+pub fn solve_puzzle_02(filename: String) -> i64 {
+    let mut increased = 0;
+
+    let input = fs::read_to_string(filename).expect("error opening file");
+
+    let mut numbers: Vec<i32> = Vec::new();
+
+    for line in input.lines() {
+        numbers.push(line.parse::<i32>().unwrap());
+    }
+
+    for (pos, _) in numbers.iter().enumerate() {
+        if pos > 0 && numbers[pos - 1] < numbers[pos] {
+                increased += 1;
+        }
+    }
+
+    increased
+}
