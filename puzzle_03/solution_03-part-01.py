3602af0fc66335d8ef9d3a5db8050fa2401d165a
idwer@localhost <idwer@localhost>
2022-07-25 11:48:13 +0200

    puzzle 03, part 01: move score counters into its scope, consolidate the rate functions and have it use a boolean to toggle between rates

--- puzzle_03/solution_03-part-01.py
+++ puzzle_03/solution_03-part-01.py
@@ -1,72 +1,49 @@
 import sys
 
 
-def get_epsilon_rate(rows: list, line_width: int) -> int:
-    epsilon_rate = 0
-
-    zeroes = 0
-    ones = 0
+def get_rate(rows: list, line_width: int, gamma: bool) -> int:
+    rate = 0
 
     for position in range(0, line_width):
-        for _, line in enumerate(rows):
-            match line[position]:
+        zeroes = 0
+        ones = 0
+
+        for row in rows:
+            match row[position]:
                 case '0':
                     zeroes += 1
                 case '1':
                     ones += 1
 
         if position > 0:
-            epsilon_rate <<= 1
+            rate <<= 1
 
-        if ones < zeroes:
-            epsilon_rate |= 1
+        if not gamma and ones < zeroes or gamma and ones > zeroes:
+            rate |= 1
 
-        ones = 0
-        zeroes = 0
-
-    return epsilon_rate
+    return rate
 
 
-def get_gamma_rate(rows: list, line_width: int) -> int:
-    gamma_rate = 0
-
-    zeroes = 0
-    ones = 0
-
-    for position in range(0, line_width):
-        for _, line in enumerate(rows):
-            match line[position]:
-                case '0':
-                    zeroes += 1
-                case '1':
-                    ones += 1
-
-        if position > 0:
-            gamma_rate <<= 1
-
-        if ones > zeroes:
-            gamma_rate |= 1
-
-        ones = 0
-        zeroes = 0
-
-    return gamma_rate
-
-
-def solution(filename: str) -> int:
-    with open(filename, 'r') as infile:
+def solution() -> int:
+    with open('input', 'r') as infile:
         rows = [n for n in infile.read().splitlines()]
         infile.close()
 
         line_width = len(rows[0])
 
-    return get_gamma_rate(rows, line_width) * get_epsilon_rate(rows, line_width)
+    return get_rate(rows, line_width, False) * get_rate(rows, line_width, True)
+
+
+def solution_sample() -> int:
+    with open('test_input', 'r') as infile:
+        rows = [n for n in infile.read().splitlines()]
+        infile.close()
+
+        line_width = len(rows[0])
+
+    return get_rate(rows, line_width, False) * get_rate(rows, line_width, True)
 
 
 if __name__ == '__main__':
-    try:
-        print(f"What is the power consumption of the submarine? {solution(sys.argv[1])}")
-    except IndexError as e:
-        print("No file name to read input from was provided")
-    except FileNotFoundError as e:
-        print(f"{sys.argv[1]}: {e.args[1]}")
+    print(f"What is the power consumption of the submarine? {solution_sample()} - using test_input")
+    print(f"What is the power consumption of the submarine? {solution()}")
