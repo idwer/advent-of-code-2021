36d48673028ff91a137e526ec086cb9d1712c5f8
idwer@localhost <idwer@localhost>
2022-07-25 18:30:03 +0200

    puzzle 03: fold puzzle solving functions

--- puzzle_03/solution_03-part-01.py
+++ puzzle_03/solution_03-part-01.py
@@ -24,18 +24,8 @@ def get_rate(rows: list, line_width: int, gamma: bool) -> int:
     return rate
 
 
-def solution() -> int:
-    with open('input', 'r') as infile:
-        rows = [n for n in infile.read().splitlines()]
-        infile.close()
-
-        line_width = len(rows[0])
-
-    return get_rate(rows, line_width, False) * get_rate(rows, line_width, True)
-
-
-def solution_sample() -> int:
-    with open('test_input', 'r') as infile:
+def solution(filename: str) -> int:
+    with open(filename, 'r') as infile:
         rows = [n for n in infile.read().splitlines()]
         infile.close()
 
@@ -45,5 +35,5 @@ def solution_sample() -> int:
 
 
 if __name__ == '__main__':
-    print(f"What is the power consumption of the submarine? {solution_sample()} - using test_input")
-    print(f"What is the power consumption of the submarine? {solution()}")
+    print(f'What is the power consumption of the submarine? {solution("test_input")} - using test_input')
+    print(f'What is the power consumption of the submarine? {solution("input")}')
