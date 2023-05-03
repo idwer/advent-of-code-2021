945f5ec69a027b18444fb1b83a47797fd1018d9a
idwer@localhost <idwer@localhost>
2023-05-03 13:54:31 +0200

    puzzle 05: rewrite the Python implementation to 1) use match syntax, 2) provide unittests, 3) use numbers instead of characters to fill the grid, 4) use PEP 0622 syntax

--- puzzle_05/solution_05.py
+++ puzzle_05/solution_05.py
@@ -1,4 +1,53 @@
-def generate_diagram(input: list) -> list:
+import unittest
+
+class TestSolution_05(unittest.TestCase):
+    def test_highest_value_from_input_sample(self):
+        rows = []
+
+        with open('test_input', 'r') as infile:
+            for line in infile.read().splitlines():
+                rows.append(line)
+
+            infile.close()
+
+        actual = highest_value_from_input(rows)
+
+        self.assertEqual(actual, 9)
+
+    def test_highest_value_from_input(self):
+        rows = []
+
+        with open('input', 'r') as infile:
+            for line in infile.read().splitlines():
+                rows.append(line)
+
+            infile.close()
+
+        actual = highest_value_from_input(rows)
+
+        self.assertEqual(actual, 990)
+
+    def test_solution_part_1_sample(self):
+        actual = solution("test_input", False)
+
+        self.assertEqual(actual, 5)
+
+    def test_solution_part_1(self):
+        actual = solution("input", False)
+
+        self.assertEqual(actual, 7269)
+
+    def test_solution_part_2_sample(self):
+        actual = solution("test_input", True)
+
+        self.assertEqual(actual, 12)
+
+    def test_solution_part_2(self):
+        actual = solution("input", True)
+
+        self.assertEqual(actual, 21140)
+
+def highest_value_from_input(input: list) -> int:
     # board dimension is the highest value of x1 or x2 or y1 or y2 + 1
     size = 0
     maximums = []
@@ -17,10 +66,14 @@ def generate_diagram(input: list) -> list:
 
         maximums.append(int(max(tmp)))
 
-    size = max(maximums)
+    return max(maximums)
 
+
+def generate_diagram(input: list) -> list:
     # add one, since arrays start at 0
-    return [['.' for x in range(size + 1)] for y in range(size + 1)]
+    size = highest_value_from_input(input) + 1
+
+    return [[0 for x in range(size)] for y in range(size)]
 
 
 def mark_diagram(diagram: list, input: list, part_two: bool) -> list:
@@ -30,9 +83,13 @@ def mark_diagram(diagram: list, input: list, part_two: bool) -> list:
         coordinates.append(e.split(' -> '))
 
     for pos in coordinates:
+        # horizontal start
         x_lefthand = int(pos[0].split(',')[0])
+        # vertical start
         y_lefthand = int(pos[0].split(',')[1])
+        # horizontal end
         x_righthand = int(pos[1].split(',')[0])
+        # vertical end
         y_righthand = int(pos[1].split(',')[1])
 
         end = 0
@@ -40,101 +97,91 @@ def mark_diagram(diagram: list, input: list, part_two: bool) -> list:
         step = 0
         
         # mark horizontal lines
-        if y_lefthand == y_righthand:
-            end = x_righthand
-            start = x_lefthand
+        match y_lefthand == y_righthand:
+            case True:
+                end = x_righthand
+                start = x_lefthand
 
-            if x_lefthand < x_righthand:
-                step = 1
-            else:
-                step = -1
+                match x_lefthand < x_righthand:
+                    case True:
+                        step = 1
+                    case False:
+                        step = -1
 
-            match step:
-                case -1:
-                    for n in range(start, end - 1, step):
-                        if diagram[y_lefthand][n] == '.':
-                            diagram[y_lefthand][n] = str(1)
-                        else:
-                            diagram[y_lefthand][n] = str(int(diagram[y_lefthand][n]) + 1)
-                case 1:
-                    for n in range(start, end + 1, step):
-                        if diagram[y_lefthand][n] == '.':
-                            diagram[y_lefthand][n] = str(1)
-                        else:
-                            diagram[y_lefthand][n] = str(int(diagram[y_lefthand][n]) + 1)
+                match step:
+                    case -1:
+                        for n in range(start, end - 1, step):
+                            diagram[y_lefthand][n] = diagram[y_lefthand][n] + 1
+                    case 1:
+                        for n in range(start, end + 1, step):
+                            diagram[y_lefthand][n] = diagram[y_lefthand][n] + 1
 
-            continue
+                continue
 
         # mark vertical lines
-        if x_lefthand == x_righthand:
-            end = y_righthand
-            start = y_lefthand
+        match x_lefthand == x_righthand:
+            case True:
+                end = y_righthand
+                start = y_lefthand
 
-            if y_lefthand < y_righthand:
-                step = 1
-            else:
-                step = -1
+                match y_lefthand < y_righthand:
+                    case True:
+                        step = 1
+                    case False:
+                        step = -1
 
-            match step:
-                case -1:
-                    for n in range(start, end - 1, step):
-                        if diagram[n][x_lefthand] == '.':
-                            diagram[n][x_lefthand] = str(1)
-                        else:
-                            diagram[n][x_lefthand] = str(int(diagram[n][x_lefthand]) + 1)
-                case 1:
-                    for n in range(start, end + 1, step):
-                        if diagram[n][x_lefthand] == '.':
-                            diagram[n][x_lefthand] = str(1)
-                        else:
-                            diagram[n][x_lefthand] = str(int(diagram[n][x_lefthand]) + 1)
+                match step:
+                    case -1:
+                        for n in range(start, end - 1, step):
+                            diagram[n][x_lefthand] = diagram[n][x_lefthand] + 1
+                    case 1:
+                        for n in range(start, end + 1, step):
+                            diagram[n][x_lefthand] = diagram[n][x_lefthand] + 1
 
-            continue
+                continue
 
         # mark diagonal lines
-        if part_two:
-            steps = 0
-            step_method = 0
-            x = x_lefthand
-            y = y_lefthand
+        match part_two:
+            case True:
+                steps = 0
+                step_method = 0
+                x = x_lefthand
+                y = y_lefthand
 
-            if x_lefthand > x_righthand and y_lefthand < y_righthand:
-                steps = x_lefthand - x_righthand
-                step_method = 1
+                if x_lefthand > x_righthand and y_lefthand < y_righthand:
+                    steps = x_lefthand - x_righthand
+                    step_method = 1
 
-            if x_lefthand > x_righthand and y_lefthand > y_righthand:
-                steps = x_lefthand - x_righthand
-                step_method = 2
+                if x_lefthand > x_righthand and y_lefthand > y_righthand:
+                    steps = x_lefthand - x_righthand
+                    step_method = 2
 
-            if x_lefthand < x_righthand and y_lefthand < y_righthand:
-                steps = x_righthand - x_lefthand
-                step_method = 3
+                if x_lefthand < x_righthand and y_lefthand < y_righthand:
+                    steps = x_righthand - x_lefthand
+                    step_method = 3
 
-            if x_lefthand < x_righthand and y_lefthand > y_righthand:
-                steps = x_righthand - x_lefthand
-                step_method = 4
+                if x_lefthand < x_righthand and y_lefthand > y_righthand:
+                    steps = x_righthand - x_lefthand
+                    step_method = 4
 
-            for n in range(0, steps + 1):
-                if diagram[y][x] == '.':
-                    diagram[y][x] = str(1)
-                else:
-                    diagram[y][x] = str(int(diagram[y][x]) + 1)
+                for n in range(0, steps + 1):
+                    diagram[y][x] = diagram[y][x] + 1
 
-                match step_method:
-                    case 1:
-                        x -= 1
-                        y += 1
-                    case 2:
-                        x -= 1
-                        y -= 1
-                    case 3:
-                        x += 1
-                        y += 1
-                    case 4:
-                        x += 1
-                        y -= 1
+                    match step_method:
+                        case 1:
+                            x -= 1
+                            y += 1
+                        case 2:
+                            x -= 1
+                            y -= 1
+                        case 3:
+                            x += 1
+                            y += 1
+                        case 4:
+                            x += 1
+                            y -= 1
 
-            continue
+                continue
 
     return diagram
 
@@ -143,8 +190,10 @@ def parse_diagram(diagram: list) -> int:
 
     for row in diagram:
         for cell in row:
-            if cell != '.' and int(cell) > 1:
-                counter += 1
+            match cell:
+            # https://peps.python.org/pep-0622/#guards
+                case n if cell > 1:
+                    counter += 1
 
     return counter
 
