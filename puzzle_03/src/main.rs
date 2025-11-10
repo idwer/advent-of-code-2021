43c64fd4aa5bb964456aecf3d79a50d0f228a348
Idwer <idwer@localhost>
2025-11-10 18:01:05 +0100

    puzzle 03: combine src/part_0?.rs into src/puzzle_03.rs

--- puzzle_03/src/main.rs
+++ puzzle_03/src/main.rs
@@ -1,12 +1,11 @@
-mod part_01;
-mod part_02;
+mod puzzle_03;
 
 fn main() {
-    let result_part_1 = part_01::solve_part_1();
-    let result_part_1_sample = part_01::solve_part_1_sample();
+    let result_part_1 = puzzle_03::solve_part_1();
+    let result_part_1_sample = puzzle_03::solve_part_1_sample();
 
-    let result_part_2 = part_02::solve_part_2();
-    let result_part_2_sample = part_02::solve_part_2_sample();
+    let result_part_2 = puzzle_03::solve_part_2();
+    let result_part_2_sample = puzzle_03::solve_part_2_sample();
 
     println!("Solution for part 1: {}", result_part_1);
     println!("Solution for part 1: {} - using test_input", result_part_1_sample);
