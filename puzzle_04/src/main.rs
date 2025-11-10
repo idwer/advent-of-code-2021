bb347585d84b27e75d4241ff8866a94c5ea3cb7e
Idwer <idwer@localhost>
2025-11-10 18:28:07 +0100

    puzzle 04: combine src/part_0?.rs into src/puzzle_04.rs

--- puzzle_04/src/main.rs
+++ puzzle_04/src/main.rs
@@ -1,26 +1,26 @@
 mod board;
-mod solution;
+mod puzzle_04;
 
 fn main() {
-    let result_part_1_sample = solution::solve_puzzle_04_sample(false);
+    let result_part_1_sample = puzzle_04::solve_puzzle_04_sample(false);
     println!(
         "Solution for part 1: {} - using test_input",
         result_part_1_sample
     );
 
-    let result_part_1 = solution::solve_puzzle_04(false);
+    let result_part_1 = puzzle_04::solve_puzzle_04(false);
     println!(
         "Solution for part 1: {} - using input",
         result_part_1
     );
 
-    let result_part_2_sample = solution::solve_puzzle_04_sample(true);
+    let result_part_2_sample = puzzle_04::solve_puzzle_04_sample(true);
     println!(
         "Solution for part 2: {} - using test_input (squid_must_win)",
         result_part_2_sample
     );
 
-    let result_part_2 = solution::solve_puzzle_04(true);
+    let result_part_2 = puzzle_04::solve_puzzle_04(true);
     println!(
         "Solution for part 2: {} - using input (squid_must_win)",
         result_part_2
