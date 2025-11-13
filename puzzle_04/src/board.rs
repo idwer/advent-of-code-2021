c5a07a404576188561fc21394571653c358dd8c2
Idwer <idwer@localhost>
2025-11-13 17:10:59 +0100

    puzzle 04: reformat data

--- puzzle_04/src/board.rs
+++ puzzle_04/src/board.rs
@@ -98,8 +98,8 @@ mod tests {
         let mut cells = Vec::new();
 
         let winning_numbers = vec![
-            7, 4, 9, 5, 11, 17, 23, 2, 0, 14, 21, 24, 10, 16, 13, 6, 15, 25, 12, 22, 18, 20, 8, 19,
-            3, 26, 1,
+            7, 4, 9, 5, 11, 17, 23, 2, 0, 14, 21, 24, 10, 16,
+            13, 6, 15, 25, 12, 22, 18, 20, 8, 19, 3, 26, 1,
         ];
 
         // first 5x5 block found in the file test_input
